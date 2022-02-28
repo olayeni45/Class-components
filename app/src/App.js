cd582a5d6d236054bf86f68bdf5ac40b0ac3c347
Olayeni Anifowose <olayeni45@gmail.com>
2022-02-28 23:25:41 +0000

    starting files

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+import Users from "./components/Users";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Users />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ app/src/components/User.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./User.module.css";
+
+const User = (props) => {
+  return <li className={styles.user}>{props.name}</li>;
+};
+
+export default User;
