08fd66480a8ab11f038841e333cfc5469fd8b97d
Olayeni Anifowose <olayeni45@gmail.com>
2022-03-01 10:33:52 +0000

    rewrite: user component

--- app/src/components/User.jsx
+++ app/src/components/User.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./User.module.css";
 
+class User extends Component {
+  render() {
+    return <li className={styles.user}>{this.props.name}</li>;
+  }
+}
+
+/*
 const User = (props) => {
   return <li className={styles.user}>{props.name}</li>;
 };
+*/
 
 export default User;
